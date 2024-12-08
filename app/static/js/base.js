f9fe7fbac5d73f5f8211df15d4b6d7da94c4241d
Minhaj Tkc <minhajtkc@gmail.com>
2024-12-08 20:54:26 +0530

    Login and Registration Pages Have Been Completed

---
+++ app/static/js/base.js
@@ -0,0 +1,5 @@
+document.getElementById('navbar-toggle').addEventListener('click', function() {
+    document.getElementById('navbar-links').classList.toggle('active');
+});
+
+console.log("Hello world");
