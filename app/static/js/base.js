80ef599261b24717a018478190c4ee87e8dfe1e0
Minhaj Tkc <minhajtkc@gmail.com>
2024-12-09 11:33:30 +0530

    split CSS into separate files for better organization

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -2,4 +2,3 @@ document.getElementById('navbar-toggle').addEventListener('click', function() {
     document.getElementById('navbar-links').classList.toggle('active');
 });
 
-console.log("Hello world");
